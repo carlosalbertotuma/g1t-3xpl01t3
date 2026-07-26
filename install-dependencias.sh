efd336d447caa3201d051012fbdb8649af1a434f
Bl4dSc4n <carlosalbertotuma@yahoo.com.br>
2026-07-26 10:39:36 -0300

    Update install-dependencias.sh

--- install-dependencias.sh
+++ install-dependencias.sh
@@ -7,7 +7,7 @@ sudo apt update -y
 echo "Instalando dependências necessárias..."
 
 # Ferramentas básicas
-sudo apt install -y curl jq
+sudo apt install -y curl jq bsdextrautils
 
 # (Opcional) Se quiser suporte a cores e column
 sudo apt install -y moreutils
@@ -18,6 +18,6 @@ echo ""
 echo "Testando versões instaladas:"
 echo -n "curl versão: "; curl --version | head -n1
 echo -n "jq versão: "; jq --version
-
+echo -n "column: ";      which column
 echo ""
 echo "Você pode agora executar o script g1t-3xpl01t3.sh normalmente."
